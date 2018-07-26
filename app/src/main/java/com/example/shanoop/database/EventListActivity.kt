1cb972894982ce869fdc5a91ddd6b706e84d488d
Manu.KS <manu.abaft@gmail.com>
2018-07-26 15:03:54 +0530

    Progressbar and Snackbar added in LoginActivity,,Menu added for signout in EventListActivity

--- app/src/main/java/com/example/shanoop/database/EventListActivity.kt
+++ app/src/main/java/com/example/shanoop/database/EventListActivity.kt
@@ -3,8 +3,11 @@ package com.example.shanoop.database
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_event_list.*
+import org.jetbrains.anko.intentFor
+
 
 class EventListActivity : AppCompatActivity() {
 
@@ -19,4 +22,21 @@ class EventListActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.action_signout->{
+                getSharedPreferences(SESSION_STORAGE,0).edit().remove(SESSION_STORAGE).apply()
+                startActivity(intentFor<LoginActivity>())
+                finish()
+            }
+        }
+        return true
+    }
 }
--- app/src/main/java/com/example/shanoop/database/LoginActivity.kt
+++ app/src/main/java/com/example/shanoop/database/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.view.View.GONE
 import kotlinx.android.synthetic.main.activity_login.*
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
@@ -21,7 +22,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (isUserLogged()) {
-            startActivity(intentFor<DashBoardActivity>())
+            startActivity(intentFor<EventListActivity>())
             finish()
         } else
             setContentView(R.layout.activity_login)
@@ -39,6 +40,9 @@ class LoginActivity : AppCompatActivity() {
             passwordEditText.error = "Invalid Password"
         if (usernameEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
             login()
+            loginButton.text="Loging In..."
+            loginProgressBar.visibility= View.VISIBLE
+            loginButton.isEnabled=false
         }
     }
 
@@ -57,26 +61,48 @@ class LoginActivity : AppCompatActivity() {
             val client = OkHttpClient()
 
             val response = client.newCall(login).execute()
+            //Log.w("LOGIN_RESP", response.body()!!.string())
 
-            Log.w("LOGIN_RESP", response.body()!!.string())
+            val result = JSONObject(response.body()!!.string())
             uiThread {
-                val result = JSONObject(response.body()!!.string())
-                when (response.code()) {
-                    200 -> {
-                        if (result.optBoolean("result", false)) {
-                            getSharedPreferences(SESSION_STORAGE, 0).edit().putString(SESSION_STORAGE, result.optString("access_token")).apply()
-                            startActivity(intentFor<DashBoardActivity>())
-                            finish()
-                        } else {
-                            Snackbar.make(root, result.optString("message"), Snackbar.LENGTH_INDEFINITE)
+                loginProgressBar.visibility=GONE
+                loginButton.isEnabled=true
+                loginButton.text="Login"
+                if(response.body()!=null)
+                {
+                    when (response.code()) {
+                        200 -> {
+                            if (result.optBoolean("result", false)) {
+                                getSharedPreferences(SESSION_STORAGE, 0).edit().putString(SESSION_STORAGE, result.optString("access_token")).apply()
+                                startActivity(intentFor<EventListActivity>())
+                                finish()
+                            } else {
+                                Snackbar.make(root2, result.optString("message"), Snackbar.LENGTH_INDEFINITE).setAction("OK",object :View.OnClickListener{
+                                    override fun onClick(p0: View?) {
+                                    }
+                                }).show()
+                            }
                         }
-                    }
-                    400 -> {
-                        Snackbar.make(root, result.optString("message"), Snackbar.LENGTH_INDEFINITE)
+                        400 -> {
+                            Snackbar.make(root2, result.optString("message"), Snackbar.LENGTH_INDEFINITE).setAction("OK", object:View.OnClickListener {
+                                override fun onClick(p0: View?) {
+                                }
+                            }).show()
+                        }
+                        else->{
+                            Snackbar.make(root2, "Something Went Wrong!", Snackbar.LENGTH_INDEFINITE).setAction("OK", object : View.OnClickListener {
+                                override fun onClick(p0: View?) {
+
+                                }
+
+                            }).show()
+                        }
                     }
                 }
 
+
             }
+
 
         }
 
